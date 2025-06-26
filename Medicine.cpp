6676869175af77d002d75eadca7f8e17850f40ed
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-26 11:15:12 +0300

    cteate pharm

--- Medicine.cpp
+++ Medicine.cpp
@@ -90,3 +90,20 @@ Medicine::Medicine(const Medicine &obj) {
 
     this->m_price = obj.m_price;
 }
+
+Medicine &Medicine::operator=(const Medicine &obj) {
+
+    int size_title = strlen(obj.m_title) + 1;
+    this->m_title = new char[size_title];
+    //strcpy_s(this->m_title, size_title, obj.m_title);
+    strcpy(this->m_title, obj.m_title);
+
+    int size_type = strlen(obj.m_type) + 1;
+    this->m_type = new char[size_type];
+    //strcpy_s(this->m_type, size_type, obj.m_type);
+    strcpy(this->m_type, obj.m_type);
+
+    this->m_price = obj.m_price;
+
+    return *this;
+}
