7dae810d199d4d78932a36bf60df8038554b5174
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-26 11:33:05 +0300

    cteate pharm

--- Pharm.h
+++ Pharm.h
@@ -8,7 +8,17 @@
 #include "Medicine.h"
 
 class Pharm {
+    char* m_name;
 
+    int m_size_medicines;
+    Medicine* m_arr;
+
+public:
+    Pharm();
+    Pharm(const char* name);
+    ~Pharm();
+
+    void showInfo()const;
 };
 
 
