0310d62cb7daeeb50c4198abd3a680a61836b0ad
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-26 11:28:23 +0300

    cteate pharm

---
+++ Pharm.h
@@ -0,0 +1,15 @@
+//
+// Created by Lapshun Tetiana on 26.06.2025.
+//
+
+#ifndef P44_T21_PHARM_PHARM_H
+#define P44_T21_PHARM_PHARM_H
+#include "Array_Template.h"
+#include "Medicine.h"
+
+class Pharm {
+
+};
+
+
+#endif //P44_T21_PHARM_PHARM_H
