4a886cfe43d75544fc48d29e94827222044fd332
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-26 10:10:29 +0300

    cteate pharm

---
+++ Medicine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lapshun Tetiana on 26.06.2025.
+//
+
+#include "Medicine.h"
---
+++ Medicine.h
@@ -0,0 +1,34 @@
+//
+// Created by Lapshun Tetiana on 26.06.2025.
+//
+
+#ifndef P44_T21_PHARM_MEDICINE_H
+#define P44_T21_PHARM_MEDICINE_H
+#include <iostream>
+#include <cstring>
+using namespace  std;
+
+class Medicine {
+private:
+    char* m_title;
+    char* m_type;
+    float m_price;
+
+public:
+    Medicine();
+    Medicine(const char* title, const char* type, float price);
+    ~Medicine();
+
+    void setTitle(const char* title);
+    void setType(const char* type);
+    void setPrice(float price);
+
+    const char* getTitle()const;
+    const char* getType()const;
+    float getPrice()const;
+
+    void showInfo()const;
+};
+
+
+#endif //P44_T21_PHARM_MEDICINE_H
